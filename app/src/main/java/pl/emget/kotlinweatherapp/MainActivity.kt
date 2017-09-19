157b6e1455c881e13075279b48516d03cf8ee4ef
Pawel Suszek <Pawel.Suszek@flir.se>
2017-09-19 12:10:54 +0200

    added json data model

--- app/src/main/java/pl/emget/kotlinweatherapp/MainActivity.kt
+++ app/src/main/java/pl/emget/kotlinweatherapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
@@ -23,8 +22,7 @@ class MainActivity : AppCompatActivity() {
         forecastList.adapter = ForecastListAdapter(itemsList)
 
         doAsync {
-            val url = "http://api.openweathermap.org/data/2.5/forecast/daily?APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
-            Request(url).run()
+            val result = Request("94043").run()
             uiThread { toast("Request completed")}
         }
     }
