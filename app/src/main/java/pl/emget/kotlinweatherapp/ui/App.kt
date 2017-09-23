9fc2f2e0ce4041b8352d08e929cdea426777e18b
Pawel Suszek <pawel.suszek2@gmail.com>
2017-09-23 15:49:44 +0200

    info about anko logger, own debug printing method as extension for Any class

--- app/src/main/java/pl/emget/kotlinweatherapp/ui/App.kt
+++ app/src/main/java/pl/emget/kotlinweatherapp/ui/App.kt
@@ -4,10 +4,11 @@ import android.app.Application
 import android.util.Log
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.debug
+import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import kotlin.properties.Delegates
 
-fun android.util.Log.dd(tag: String = "${javaClass.simpleName}", message: String) = Log.d(tag, message)
+fun Any.dd(message: String, tag: String = "${this.javaClass.simpleName}") = Log.d(tag, message)
 
 class App : Application(), AnkoLogger {
 
@@ -23,8 +24,14 @@ class App : Application(), AnkoLogger {
         myProp2 = 67
         myProp2 = -99 // this is never assigned because delegate forbids value less than 0
         Log.d("aa", myProp2.toString())
-        debug { myProp2 }
-
+        /*
+        ANKO will not show any lower level than INFO:
+        To view verbose level log during development, run this command from adb shell.
+        setprop  log.tag.MyApp  VERBOSE
+        End users will not see the log entries since the default log level is INFO.
+         */
+        info { myProp2 }
+        dd("msggg from ddd")
     }
 
     var myProp by Delegates.observable("") { _, _, new ->
